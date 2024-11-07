cf41084a9c7c8a656aad396c377714b8a0d93466
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2024-11-07 13:37:16 +0100

    update roll pass

--- pyroll/sander_spreading.py
+++ pyroll/sander_spreading.py
@@ -1,38 +1,38 @@
 import numpy as np
 
-from pyroll.core import BaseRollPass, root_hooks, Unit
+from pyroll.core import SymmetricRollPass, root_hooks, Unit
 from pyroll.core.hooks import Hook
 
 VERSION = "3.0.0"
 
-BaseRollPass.sander_temperature_coefficient = Hook[float]()
+SymmetricRollPass.sander_temperature_coefficient = Hook[float]()
 """Temperature correction factor a for Sander's spread equation."""
 
-BaseRollPass.sander_velocity_coefficient = Hook[float]()
+SymmetricRollPass.sander_velocity_coefficient = Hook[float]()
 """Velocity correction factor c for Sander's spread equation."""
 
-BaseRollPass.sander_material_coefficient = Hook[float]()
+SymmetricRollPass.sander_material_coefficient = Hook[float]()
 """Material correction factor d for Sander's spread equation."""
 
-BaseRollPass.sander_friction_coefficient = Hook[float]()
+SymmetricRollPass.sander_friction_coefficient = Hook[float]()
 """Friction correction factor f for Sander's spread equation."""
 
-BaseRollPass.sander_exponent = Hook[float]()
+SymmetricRollPass.sander_exponent = Hook[float]()
 """Exponent w for for Sander's spread equation."""
 
 root_hooks.add(Unit.OutProfile.width)
 
 
-@BaseRollPass.sander_temperature_coefficient
-def sander_temperature_coefficient(self: BaseRollPass):
+@SymmetricRollPass.sander_temperature_coefficient
+def sander_temperature_coefficient(self: SymmetricRollPass):
     if self.in_profile.temperature >= (950 + 273.15):
         return 1
     else:
         return 1.005
 
 
-@BaseRollPass.sander_velocity_coefficient
-def sander_velocity_coefficient(self: BaseRollPass):
+@SymmetricRollPass.sander_velocity_coefficient
+def sander_velocity_coefficient(self: SymmetricRollPass):
     if self.velocity <= 60:
         return 1 - 0.0033 * self.velocity * (
                 1 - 1 / (self.out_profile.equivalent_width / self.in_profile.equivalent_width))
@@ -40,18 +40,18 @@ def sander_velocity_coefficient(self: BaseRollPass):
         return 1
 
 
-@BaseRollPass.sander_material_coefficient
-def sander_material_coefficient(self: BaseRollPass):
+@SymmetricRollPass.sander_material_coefficient
+def sander_material_coefficient(self: SymmetricRollPass):
     return 1
 
 
-@BaseRollPass.sander_friction_coefficient
-def sander_friction_coefficient(self: BaseRollPass):
+@SymmetricRollPass.sander_friction_coefficient
+def sander_friction_coefficient(self: SymmetricRollPass):
     return 1
 
 
-@BaseRollPass.sander_exponent
-def sander_exponent(self: BaseRollPass):
+@SymmetricRollPass.sander_exponent
+def sander_exponent(self: SymmetricRollPass):
     equivalent_height_change = self.in_profile.equivalent_height - self.out_profile.equivalent_height
 
     return 10 ** (
@@ -61,8 +61,8 @@ def sander_exponent(self: BaseRollPass):
     )
 
 
-@BaseRollPass.OutProfile.width
-def width(self: BaseRollPass.OutProfile):
+@SymmetricRollPass.OutProfile.width
+def width(self: SymmetricRollPass.OutProfile):
     rp = self.roll_pass
 
     if not self.has_set_or_cached("width"):
